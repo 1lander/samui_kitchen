3b9e141b8f1d56432b12d4dd15e78e29cfaec0b2
lander <lander.vanlandeghem@student.ap.be>
2025-04-23 19:47:13 +0200

    larger mobile menu icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -86,7 +86,7 @@
             <svg
               v-if="!isMobileMenuOpen"
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              class="h-8 w-8"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -96,7 +96,7 @@
             <svg
               v-else
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              class="h-8 w-8"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"

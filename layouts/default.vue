a030cc41419256486dc1981d4983bc8eac712f78
lander <lander.vanlandeghem@student.ap.be>
2025-04-23 23:54:08 +0200

    small change

--- layouts/default.vue
+++ layouts/default.vue
@@ -83,7 +83,6 @@
           <!-- Mobile menu button -->
           <button class="relative z-40 text-primary md:hidden" @click="toggleMobileMenu">
             <svg
-              v-if="!isMobileMenuOpen"
               xmlns="http://www.w3.org/2000/svg"
               class="mr-1 h-8 w-8"
               fill="none"
@@ -91,16 +90,6 @@
               stroke="currentColor"
             >
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-            <svg
-              v-else
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-8 w-8"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
